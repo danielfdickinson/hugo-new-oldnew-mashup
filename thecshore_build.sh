9920514489745747b1dfc5cc04fb71a2d7d81918
Daniel F. Dickinson <cshored@thecshore.com>
2019-08-31 23:53:39 -0400

    Fix production builds and demo site requirements
    Make sure we have BaseURL set to https://example.com as per hugo theme
    site requirements, but that our 'production' builds use our actual
    location.
    
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- thecshore_build.sh
+++ thecshore_build.sh
@@ -2,7 +2,8 @@
 
 cd exampleSite && {
 	mkdir -p themes || exit 1
-	ln -sf ../.. themes/oldnew-mashup || exit 1
-	sed -i -e 's,BaseURL = '\''https://example.com/'\'',BaseURL = '\''https://new-oldnew-mashup.thecshore.com/'\'',' config.toml
-	hugo || exit 1
+	if [ ! -d themes/oldnew-mashup ]; then
+		ln -sf ../.. themes/oldnew-mashup || exit 1
+	fi
+	hugo -b https://new-oldnew-mashup.thecshore.com/ || exit 1
 } || exit 1
