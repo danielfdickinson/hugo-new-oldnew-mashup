79ed0d38b62045b3d05f4a51b22c12c850b9ab04
Daniel F. Dickinson <cshored@thecshore.com>
2020-03-15 20:52:28 -0400

    Fix hugo version (extended & 0.66.0)
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- ci/travis-cmd.sh
+++ ci/travis-cmd.sh
@@ -5,8 +5,9 @@ mkdir -p exampleSite/themes
 ( cd exampleSite/themes && ln -s ../.. oldnew-mashup )
 mkdir binaries
 cd binaries
-wget https://github.com/gohugoio/hugo/releases/download/v0.65.3/hugo_0.65.3_Linux-64bit.tar.gz
-tar -xzf hugo_0.65.3_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.66.0/hugo_extended_0.66.0_Linux-64bit.tar.gz
+tar -xzf hugo_extended_0.66.0_Linux-64bit.tar.gz
+mv hugo hugo-extended
 cd ..
 export PATH=$(pwd)/binaries:./node_modules/.bin:$PATH
 npm install
